100a4e622f07a8b5fc3632fc9ac967184f2aca56
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-09-10 18:36:55 -0400

    use cody to add subclasses

--- Pet.js
+++ Pet.js
@@ -10,9 +10,28 @@ class Pet {
   }
 }
 
-const dog = new Pet("CatDog", 4);
-const cat = new Pet("CatDog", 4);
+class Dog extends Pet {
+  constructor(name, age) {
+    super(name, age);
+  }
+  bark() {
+    console.log(`${this.name} says "Woof!"`);
+  }
+}
+
+class Cat extends Pet {
+  constructor(name, age) {
+    super(name, age);
+  }
+  meow() {
+    console.log(`${this.name} says "Meow!"`);
+  }
+}
+
+const dog = new Dog("Buddy", 4);
+const cat = new Cat("Whiskers", 3);
 
 console.log(welcomeMessage);
-dog.speak(`${dog.name} says "Hi-ho diggety!!"`);
-cat.speak(`${dog.name} says "Bee-yoo-tee-ful!"`);
+
+dog.bark();
+cat.meow();
